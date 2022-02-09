cd56cf0b4f4ad83c11fb85172cbd31c72060a376
Belen montoya <belenmontoya@MacBook-Pro-de-Belen.local>
2022-02-08 21:55:09 -0300

    datepicker added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6e5374225644f26a"; }
+/******/ 		__webpack_require__.h = function() { return "d3ecca85691ffaaf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
