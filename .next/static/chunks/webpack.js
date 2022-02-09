1928364be87eae7955bf58b0371f6f216b1f2e09
Belen montoya <belenmontoya@MacBook-Pro-de-Belen.local>
2022-02-08 22:04:59 -0300

    modify the daterangepicker

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d3ecca85691ffaaf"; }
+/******/ 		__webpack_require__.h = function() { return "600ca716504d6997"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.92f1fa882c4107e0.hot-update.js
+++ .next/static/webpack/webpack.92f1fa882c4107e0.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f00848139f7a1bc0"; }
+/******/ 	__webpack_require__.h = function() { return "ab9228724bd95206"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
