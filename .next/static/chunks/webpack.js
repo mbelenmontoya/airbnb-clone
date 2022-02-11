609639e9144fbcf241f9d033bbb5497dfe7c00dd
Belen montoya <belenmontoya@192.168.1.4>
2022-02-11 02:41:16 -0300

    implement bookings

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eb7d923064ad62b2"; }
+/******/ 		__webpack_require__.h = function() { return "6bc16b5e88d68081"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,37 +1,44 @@
-import { useState, useEffect } from "react"
-import Head from "next/head"
-import Layout from "../../components/Layout"
-import DateRangePicker from "../../components/DateRangePicker"
-import { useStoreActions } from "easy-peasy"
-import Cookies from 'cookies'
-import { House as HouseModel } from '../../model.js'
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Layout from "../../components/Layout";
+import DateRangePicker from "../../components/DateRangePicker";
+import { useStoreActions } from "easy-peasy";
+import Cookies from "cookies";
+import { House as HouseModel } from "../../model.js";
+import { useStoreState } from "easy-peasy";
+import axios from "axios";
 
 const calcNumberOfNightsBetweenDates = (startDate, endDate) => {
-  const start = new Date(startDate) //clone
-  const end = new Date(endDate) //clone
-  let dayCount = 0
+  const start = new Date(startDate); //clone
+  const end = new Date(endDate); //clone
+  let dayCount = 0;
 
   while (end > start) {
-    dayCount++
-    start.setDate(start.getDate() + 1)
+    dayCount++;
+    start.setDate(start.getDate() + 1);
   }
 
-  return dayCount
+  return dayCount;
 };
 
 export default function House({ house, nextbnb_session }) {
-  const [dateChosen, setDateChosen] = useState(false)
+  const [dateChosen, setDateChosen] = useState(false);
   const [numberOfNightsBetweenDates, setNumberOfNightsBetweenDates] =
-    useState(0)
+    useState(0);
   const setShowLoginModal = useStoreActions(
     (actions) => actions.modals.setShowLoginModal
-  )
-  const setLoggedIn = useStoreActions((actions) => actions.login.setLoggedIn)
+  );
+  const setLoggedIn = useStoreActions((actions) => actions.login.setLoggedIn);
   useEffect(() => {
     if (nextbnb_session) {
-      setLoggedIn(true)
+      setLoggedIn(true);
     }
-  }, [])
+  }, []);
+
+  const loggedIn = useStoreState((state) => state.login.loggedIn);
+
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
 
   return (
     <Layout
@@ -41,7 +48,11 @@ export default function House({ house, nextbnb_session }) {
             <title>{house.title}</title>
           </Head>
           <article>
-            <img src={ `/img/${house.picture}` } width="100%" alt="House picture" />
+            <img
+              src={`/img/${house.picture}`}
+              width="100%"
+              alt="House picture"
+            />
             <p>
               {house.type} - {house.town}
             </p>
@@ -55,6 +66,8 @@ export default function House({ house, nextbnb_session }) {
                   calcNumberOfNightsBetweenDates(startDate, endDate)
                 );
                 setDateChosen(true);
+                setStartDate(startDate);
+                setEndDate(endDate);
               }}
             />
             {dateChosen && (
@@ -62,17 +75,40 @@ export default function House({ house, nextbnb_session }) {
                 <h2>Price per night</h2>
                 <p>${house.price}</p>
                 <h2>Total price for booking</h2>
-                <p>
-                  ${(numberOfNightsBetweenDates * house.price).toFixed(2)}
-                </p>
-                <button
-                  className="reserve"
-                  onClick={() => {
-                    setShowLoginModal();
-                  }}
-                >
-                  Reserve
-                </button>
+                <p>${(numberOfNightsBetweenDates * house.price).toFixed(2)}</p>
+                {loggedIn ? (
+                  <button
+                    className="reserve"
+                    onClick={async () => {
+                      try {
+                        const response = await axios.post("/api/reserve", {
+                          houseId: house.id,
+                          startDate,
+                          endDate,
+                        });
+                        if (response.data.status === "error") {
+                          alert(response.data.message);
+                          return;
+                        }
+                        console.log(response.data);
+                      } catch (error) {
+                        console.log(error);
+                        return;
+                      }
+                    }}
+                  >
+                    Reserve
+                  </button>
+                ) : (
+                  <button
+                    className="reserve"
+                    onClick={() => {
+                      setShowLoginModal();
+                    }}
+                  >
+                    Log in to Reserve
+                  </button>
+                )}
               </div>
             )}
           </aside>
@@ -95,16 +131,16 @@ export default function House({ house, nextbnb_session }) {
 }
 
 export async function getServerSideProps({ req, res, query }) {
-  const { id } = query
-  const cookies = new Cookies(req, res)
-  const nextbnb_session = cookies.get('nextbnb_session')
+  const { id } = query;
+  const cookies = new Cookies(req, res);
+  const nextbnb_session = cookies.get("nextbnb_session");
 
-  const house = await HouseModel.findByPk(id)
+  const house = await HouseModel.findByPk(id);
 
   return {
     props: {
       house: house.dataValues,
-      nextbnb_session: nextbnb_session || null
+      nextbnb_session: nextbnb_session || null,
     },
-  }
+  };
 }
